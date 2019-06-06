a1ce95fed4132a40cf276c81c90ccccb7b5cdb89
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-06 14:26:38 -0400

    Set up basic Spring project.
    This is the result of running the Spring initializer, then belatedly adding the
    MySQL and H2 dependencies, and merging the .gitignore it provides with the one
    provided in the initial commit in this repository.

---
+++ src/main/java/com/sst/utopia/booking/UtopiaBookingApplication.java
@@ -0,0 +1,13 @@
+package com.sst.utopia.booking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtopiaBookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtopiaBookingApplication.class, args);
+	}
+
+}
