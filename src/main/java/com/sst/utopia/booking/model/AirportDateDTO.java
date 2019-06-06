eb75edae74189b901ce9835a544a0f173e071e98
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-06 17:34:41 -0400

    Add Flight model class, and airport-date pair DTO class it embeds.

---
+++ src/main/java/com/sst/utopia/booking/model/AirportDateDTO.java
@@ -0,0 +1,73 @@
+package com.sst.utopia.booking.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+import javax.persistence.Embeddable;
+
+/**
+ * A particular airport at a particular date. This class only exists because
+ * this combination occurs twice in a Flight object.
+ *
+ * @author Jonathan Lovelace
+ *
+ */
+@Embeddable
+public class AirportDateDTO {
+	/**
+	 * The airport that the flight is departing from or arriving at.
+	 */
+	private Airport airport;
+	/**
+	 * The date and time that the flight is expected to depart or arrive.
+	 */
+	private LocalDateTime date;
+
+	/**
+	 * @return the airport that the flight is departing from or arriving at.
+	 */
+	public Airport getAirport() {
+		return airport;
+	}
+
+	/**
+	 * @return the date and time that the flight is expected to depart or arrive.
+	 */
+	public LocalDateTime getDate() {
+		return date;
+	}
+
+	/**
+	 * An object is equal to this one iff it is the same airport and date (including
+	 * time).
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj instanceof AirportDateDTO) {
+			return Objects.equals(airport, ((AirportDateDTO) obj).getAirport())
+					& Objects.equals(date, ((AirportDateDTO) obj).getDate());
+		} else {
+			return false;
+		}
+	}
+
+	/**
+	 * We calculate our hash code based on the two fields.
+	 */
+	@Override
+	public int hashCode() {
+		return Objects.hash(airport, date);
+	}
+
+	/**
+	 * @return a String representation of this object.
+	 */
+	@Override
+	public String toString() {
+		return String.format("%s (%s) at %s %s", airport.getCode(),
+				airport.getName(), date.toLocalDate().toString(),
+				date.toLocalTime().toString());
+	}
+}
---
+++ src/main/java/com/sst/utopia/booking/model/Flight.java
@@ -0,0 +1,121 @@
+package com.sst.utopia.booking.model;
+
+import java.util.Objects;
+
+import javax.persistence.AttributeOverride;
+import javax.persistence.AttributeOverrides;
+import javax.persistence.Column;
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+/**
+ * A flight the airline is offering.
+ *
+ * While I don't expect instances of this class to be modified by user code
+ * (since an admin interface is out of the scope of the Utopia Airlines project,
+ * let alone the Booking service), and so have not provided setters, fields are
+ * left non-final for the JPA.
+ *
+ * @author Jonathan Lovelace
+ */
+@Entity
+@Table(name = "tbl_flights")
+public class Flight {
+	/**
+	 * The flight's ID number in the database.
+	 */
+	@Id
+	@Column
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	/**
+	 * The airport and date and time of departure.
+	 */
+	@Embedded
+	@AttributeOverrides({
+			@AttributeOverride(name = "airport", column = @Column(name = "departure")),
+			@AttributeOverride(name = "date", column = @Column(name = "departure_date")) })
+	private AirportDateDTO departure;
+
+	/**
+	 * The airport and date and time of arrival.
+	 */
+	@Embedded
+	@AttributeOverrides({
+		@AttributeOverride(name = "airport", column = @Column(name = "destination")),
+		@AttributeOverride(name = "date", column = @Column(name = "arrival_date"))
+	})
+	private AirportDateDTO arrival;
+
+	/**
+	 * The flight number that users will see. This is intended to be a hash derived
+	 * from the ID number and the departure airport (and perhaps the destination
+	 * airport), stored to speed the performance of queries filtering on it.
+	 */
+	@Column
+	private int flightNumber;
+
+	/**
+	 * @return the internal ID number of the flight
+	 */
+	public int getId() {
+		return id;
+	}
+
+	/**
+	 * @return the location and planned date and time of departure
+	 */
+	public AirportDateDTO getDeparture() {
+		return departure;
+	}
+
+	/**
+	 * @return the location and planned date and time of arrival
+	 */
+	public AirportDateDTO getArrival() {
+		return arrival;
+	}
+
+	/**
+	 * @return the consumer-visible "flight number"
+	 */
+	public int getFlightNumber() {
+		return flightNumber;
+	}
+
+	/**
+	 * We use the primary-key ID number as our hash value.
+	 */
+	@Override
+	public int hashCode() {
+		return id;
+	}
+
+	/**
+	 * An object is equal iff it is a Flight with the same ID and equal arrival and
+	 * departure.
+	 */
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj) {
+			return true;
+		} else if (obj instanceof Flight) {
+			return id == ((Flight) obj).getId()
+					&& Objects.equals(departure, ((Flight) obj).getDeparture())
+					&& Objects.equals(arrival, ((Flight) obj).getArrival());
+		} else {
+			return false;
+		}
+	}
+
+	@Override
+	public String toString() {
+		return String.format("Flight %d (internal #%d) from %s to %s", flightNumber,
+				id, departure.toString(), arrival.toString());
+	}
+}
