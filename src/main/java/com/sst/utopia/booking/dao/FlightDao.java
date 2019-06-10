925e0604c473dd3f963771accc9e85e1abb623b2
Jonathan Lovelace <kingjon3377@gmail.com>
2019-06-10 16:54:54 -0400

    FlightDao: Add findByFlightNumber method.

--- src/main/java/com/sst/utopia/booking/dao/FlightDao.java
+++ src/main/java/com/sst/utopia/booking/dao/FlightDao.java
@@ -1,5 +1,7 @@
 package com.sst.utopia.booking.dao;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,4 +13,11 @@ import com.sst.utopia.booking.model.Flight;
  * @author Jonathan Lovelace
  */
 @Repository
-public interface FlightDao extends JpaRepository<Flight, Integer> {}
+public interface FlightDao extends JpaRepository<Flight, Integer> {
+	/**
+	 * Get the flight, if any, with the given flight number.
+	 * @param flightNumber the flight number
+	 * @return the flight with that number, if any
+	 */
+	List<Flight> findByFlightNumber(int flightNumber);
+}
